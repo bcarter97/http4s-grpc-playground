07b4f0b6c01e3e8f9d629d24dc2d9bfd10e7cbaf
Ben Carter <ben.carter@sky.uk>
2026-04-24 13:31:45 +0100

    Add debug logging

--- fs2-grpc/src/main/scala/io/github/bcarter97/http4sgrpc/server/GreeterServer.scala
+++ fs2-grpc/src/main/scala/io/github/bcarter97/http4sgrpc/server/GreeterServer.scala
@@ -1,23 +1,26 @@
 package io.github.bcarter97.http4sgrpc.server
 
 import cats.Applicative
+import cats.effect.std.Console
 import cats.syntax.all.*
 import fs2.Stream
 import io.github.bcarter97.greeter.{GreeterFs2Grpc, HelloReply, HelloRequest}
 import io.grpc.Metadata
 
-final class GreeterServer[F[_] : Applicative] extends GreeterFs2Grpc[F, Metadata] {
+final class GreeterServer[F[_] : Applicative : Console] extends GreeterFs2Grpc[F, Metadata] {
 
   override def sayHello(request: HelloRequest, ctx: Metadata): F[HelloReply] =
-    response(request).pure
+    response(request)
 
   override def sayHelloStream(request: HelloRequest, ctx: Metadata): Stream[F, HelloReply] =
-    Stream(response(request))
+    Stream.eval(response(request))
 
   override def sayHelloStreamToStream(request: Stream[F, HelloRequest], ctx: Metadata): Stream[F, HelloReply] =
-    request.map(response)
+    request.evalMap(response)
 
   private def response(request: HelloRequest) =
-    HelloReply(message = s"Hello, ${request.name}!")
+    Console[F]
+      .println(s"Received: ${request.name}")
+      .as(HelloReply(message = s"Hello, ${request.name}!"))
 
 }
--- http4s-grpc/src/main/scala/io/github/bcarter97/http4sgrpc/server/GreeterServer.scala
+++ http4s-grpc/src/main/scala/io/github/bcarter97/http4sgrpc/server/GreeterServer.scala
@@ -1,23 +1,26 @@
 package io.github.bcarter97.http4sgrpc.server
 
 import cats.Applicative
+import cats.effect.std.Console
 import cats.syntax.all.*
 import fs2.Stream
 import io.github.bcarter97.greeter.{Greeter, HelloReply, HelloRequest}
 import org.http4s.Headers
 
-final class GreeterServer[F[_] : Applicative] extends Greeter[F] {
+final class GreeterServer[F[_] : Applicative : Console] extends Greeter[F] {
 
   override def sayHello(request: HelloRequest, ctx: Headers): F[HelloReply] =
-    response(request).pure
+    response(request)
 
   override def sayHelloStream(request: HelloRequest, ctx: Headers): Stream[F, HelloReply] =
-    Stream(response(request))
+    Stream.eval(response(request))
 
   override def sayHelloStreamToStream(request: Stream[F, HelloRequest], ctx: Headers): Stream[F, HelloReply] =
-    request.map(response)
+    request.evalMap(response)
 
   private def response(request: HelloRequest) =
-    HelloReply(message = s"Hello, ${request.name}!")
+    Console[F]
+      .println(s"Received: ${request.name}")
+      .as(HelloReply(message = s"Hello, ${request.name}!"))
 
 }
